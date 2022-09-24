ff1454226db351f723e78f673eb1d8860f6261f4
RoxanaAnamariaTurc <roxi.anamaria25@gmail.com>
2022-09-24 10:26:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import { Component } from 'react';
+
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      name: 'Roxi'
+    }
+  }
+
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button onClick={() =>
+          {
+            this.setState(this.state.name === 'Roxi' ? { name: 'Andrei' } : { name: 'Roxi' });
+
+
+            // if (this.state.name === 'Roxi')
+            // {
+            //   this.setState({ name: 'Andrei' })
+            // } else
+            // {
+            //   this.setState({ name: 'Roxi' })
+            // }
+
+          }}>
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
