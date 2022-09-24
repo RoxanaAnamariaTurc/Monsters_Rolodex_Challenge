813abf436392ad0c47719a0038e8ce07e29e74a4
RoxanaAnamariaTurc <roxi.anamaria25@gmail.com>
2022-09-24 14:47:28 +0100

    Added search box component and filtering functionality

--- src/App.js
+++ src/App.js
@@ -10,22 +10,76 @@ class App extends Component
   {
     super();
     this.state = {
-      name: 'Roxi'
+      monsters: [],
+      searchField: ''
+
     }
+  }
+
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) =>
+        response.json())
+      .then((users) => this.setState(() =>
+      {
+        return { monsters: users }
+      },
+        () =>
+        {
+          console.log(this.state)
+        }))
   }
 
   render()
   {
+    const filteredMonsters = this.state.monsters.filter((monster) =>
+    {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    });
     return (
       <div className="App">
-        <header className="App-header">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) =>
+        {
+          const searchField = event.target.value.toLocaleLowerCase();
+
+          this.setState(() =>
+          {
+            return { searchField };
+          })
+
+        }} />
+        {
+
+          filteredMonsters.map((monster) =>
+          {
+            return (
+              <div key={monster.id}>
+                <h1> {monster.name}</h1>
+              </div>
+            )
+
+          })
+        }
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstname} {this.state.name.lastname}, I work at {this.state.company}
+
           </p>
           <button onClick={() =>
           {
-            this.setState(this.state.name === 'Roxi' ? { name: 'Andrei' } : { name: 'Roxi' });
+            this.setState(() =>
+            {
+              return {
+                name: { firstname: 'Andrei', lastname: 'Neagoie' },
+              }
+              // }, () =>
+              // {
+              //   console.log(this.state)
+            });
+            // this.setState(this.state.name.firstname === 'Roxi' ? { name: { firstname: 'Andrei', lastname: 'Neagoie' } } : { name: { firstname: 'Roxi', lastname: 'Turc' } });
 
 
             // if (this.state.name === 'Roxi')
@@ -39,7 +93,7 @@ class App extends Component
           }}>
             Change name
           </button>
-        </header>
+        </header> */}
       </div>
     );
 
