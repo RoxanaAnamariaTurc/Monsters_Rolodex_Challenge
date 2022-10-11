54af83709711052f0ecf55c23899c8bdb2f51296
RoxanaAnamariaTurc <roxi.anamaria25@gmail.com>
2022-10-11 09:27:31 +0100

    Changed the program from using class components to functional components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,18 @@
-import { Component } from 'react';
 
 import Card from '../card/card.component';
 import './card-list.style.css';
 
-class CardList extends Component
+const CardList = ({ monsters }) => 
 {
-    render()
-    {
-        const { monsters } = this.props;
-
-        return (
-            <div className='card-list'>
-                {monsters.map((monster) =>
-                {
-                    return <Card monster={monster} />
-                })}
-            </div>
-        )
-    }
+    return (
+        <div className='card-list'>
+            {monsters.map((monster) =>
+            {
+                return <Card monster={monster} />
+            })}
+        </div>
+    )
 }
+
 
 export default CardList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,16 @@
-import { Component } from 'react';
 import './search-box.style.css';
 
-class SearchBox extends Component
+const SearchBox = ({ className, placeholder, onChangeHandler }) =>
 {
-    render()
-    {
-        return (
-            <input
-                className={`search-box ${this.props.className} `}
-                type='search'
-                placeholder={this.props.placeholder}
-                onChange={this.props.onChangeHandler} />
-        )
-    }
+
+    return (
+        <input
+            className={`search-box ${className} `}
+            type='search'
+            placeholder={placeholder}
+            onChange={onChangeHandler} />
+    )
 }
+
 
 export default SearchBox;
